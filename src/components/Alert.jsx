80cfad86480c448859558d8f61c7706d9729ed8e
AnastAnt <141515437+AnastAnt@users.noreply.github.com>
2024-03-13 17:47:16 +0100

    я

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,15 +1,2 @@
 import clsx from "clsx";
 import css from "./Alert.module.css";
-
-const Alert = ({ variant, outlined, elevated, children }) => {
-  return (
-    <p
-      className={clsx(css[variant], {
-        [css.isOutlined]: outlined,
-        [css.isElevated]: elevated,
-      })}
-    >
-      {children}
-    </p>
-  );
-};
