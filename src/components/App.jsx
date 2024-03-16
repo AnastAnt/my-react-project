67e909079baac37467e38d53328cb0fe1ada9b22
AnastAnt <141515437+AnastAnt@users.noreply.github.com>
2024-03-16 14:33:34 +0100

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,20 @@
-import { Product } from "./Product";
+const Product = () => {
+  return (
+    <div>
+      <h2>Cookies</h2>
+      <p>Price: 999 credits</p>
+    </div>
+  );
+};
 
 export default function App() {
   return (
     <div>
-      <h1>Best selling</h1>
+      <h1>Products</h1>
 
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-      />
+      <Product />
+      <Product />
+      <Product />
     </div>
   );
 }
-
-import { Alert } from "./Alert";
-
-const App = () => {
-  return (
-    <>
-      <Alert variant="info">
-        Would you like to browse our recommended products?
-      </Alert>
-      <Alert variant="error">
-        There was an error during your last transaction
-      </Alert>
-      <Alert variant="success">
-        Payment received, thank you for your purchase
-      </Alert>
-      <Alert variant="warning">
-        Please update your profile contact information
-      </Alert>
-    </>
-  );
-};
